08a01c555ae463b9e141772944de57130626e2c9
gameingame-eng <231358369+gameingame-eng@users.noreply.github.com>
2026-03-26 17:26:03 -0500

    change target temp

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let users = new Map();
 input.addEventListener('blur', () => input.focus());
 input.focus();
 
-const socket = new WebSocket("wss://chat.waffledogz.us/ws");
+const socket = new WebSocket("wss://localhost:8080/ws");
 
 socket.addEventListener("error", (e) => alert("WS error: " + e));
 socket.addEventListener("close", (e) => alert("WS closed: " + e.code + " " + e.reason));
