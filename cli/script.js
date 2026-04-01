b2faef1cd6ac1aa553895e08d001ea26b4035c39
gameingame-eng <231358369+gameingame-eng@users.noreply.github.com>
2026-03-31 19:59:11 -0500

    Revert "fixing phone"
    This reverts commit cecd2ce8146c7f5a8105f89bdd0342f58fbab347.

--- cli/script.js
+++ cli/script.js
@@ -130,7 +130,7 @@ input.addEventListener('blur', () => input.focus());
 input.focus();
 
 const wsProtocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-const wsHost = window.location.host || "chat.waffledogz.us";
+const wsHost = "localhost:8080";
 const wsUrl = `${wsProtocol}//${wsHost}/ws`;
 const socket = new WebSocket(wsUrl);
 let movedToErrorPage = false;
